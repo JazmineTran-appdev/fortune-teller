b1b01c5ce0fd740fde2371dba959f3639cbe411d
Jazmine Tran <jazminetra00@gmail.com>
2023-03-28 20:24:50 +0000

    RCAV 2d8

--- app/controllers/eight_controller.rb
+++ app/controllers/eight_controller.rb
@@ -9,4 +9,15 @@ class EightController < ApplicationController
 
     render({ :template => "eight_view/one_eight.html.erb"})
   end
+
+  def two_eight
+    @dice = Array.new
+
+    2.times do
+      roll = rand(1..8)
+      @dice.push(roll)
+    end
+
+    render({ :template => "eight_view/two_eight.html.erb"})
+  end
 end
