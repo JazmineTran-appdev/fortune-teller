cdcbea5c112a663f11265aa4ecd509796631068a
Jazmine Tran <jazminetra00@gmail.com>
2023-03-28 18:47:10 +0000

    debugged unlucky numbers

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -6,8 +6,21 @@ class NumbersController < ApplicationController
       giraffe = rand(1...100)
       
       @zebra.push(giraffe)
-    end
+    end 
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/boohoo.html.erb"})
+  end
+
 end
